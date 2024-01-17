f47cd379a2cfe67198af5c43a32b19bcd23a3861
Kalel L. Martinho <kalel@tutamail.com>
2024-01-17 17:23:15 -0300

    feat: Add database

---
+++ internal/tools/database.go
@@ -0,0 +1,31 @@
+package tools
+
+import log "github.com/sirupsen/logrus"
+
+type LoginDetails struct {
+	Username  string
+	AuthToken string
+}
+
+type TodoDetails struct {
+	Title       string
+	Description string
+	Username    string
+}
+
+type DatabaseInterface interface {
+	GetLoginDetails(username string) *LoginDetails
+	GetTodos(username string) *[]TodoDetails
+	SetupDatabase() error
+}
+
+func NewDatabase() (*DatabaseInterface, error) {
+	var database DatabaseInterface = &mockDatabase{}
+	var err error = database.SetupDatabase()
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	return &database, nil
+}
