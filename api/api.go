a5566d3de3c6b63b6a344df2dddee2a132485eb9
Kalel L. Martinho <kalel@tutamail.com>
2024-01-17 16:47:06 -0300

    feat: Add structs

--- api/api.go
+++ api/api.go
@@ -1,1 +1,19 @@
 package api
+
+type TodoParams struct {
+	Username string
+}
+
+type Todo struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type TodoResponse struct {
+	Todos []Todo
+}
+
+type Error struct {
+	Code    int
+	Message string
+}
